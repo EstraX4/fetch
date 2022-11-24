de89bfcaa26339a13a331e3586677138ce1ecf83
EstraX4 <EstraX4@users.noreply.github.com>
2022-11-24 21:46:08 +0000

    deploy: a45b9a3aa1193ab38942af97664bcac9bc715b74

--- _dist_/index.js
+++ _dist_/index.js
@@ -1,24 +1,30 @@
-const url = "https://platzi-avo.vercel.app/api/avo";
+const baseUrl ="https://platzi-avo.vercel.app"
+
+const appNode = document.querySelector("#app")
 
 //web api
 //Conectamos al server
 window
-.fetch(url)
-//procesar la respuesta y convertirla en JSON
-.then((respuesta) => respuesta.json())
-// JSON -> Data -> Renderizar info browser
-.then((responseJson) => {
-    const todosLosItems = []
+  .fetch(`${baseUrl}/api/avo`)
+  //procesar la respuesta y convertirla en JSON
+  .then((respuesta) => respuesta.json())
+  // JSON -> Data -> Renderizar info browser
+  .then((responseJson) => {
+    const todosLosItems = [];
     responseJson.data.forEach((item) => {
-        const imagen = document.createElement("img");
-        const title = document.createElement("h2");
-        const price = document.createElement("div");
+      const imagen = document.createElement("img");
+        imagen.src = `${baseUrl}${item.image}`;
 
-        const container = document.createElement("div");
-        container.append(imagen, title, price);
+      const title = document.createElement("h2");
+      title.textContent = item.name;
 
-        todosLosItems.push(container);
+      const price = document.createElement("div");
+      price.textContent = item.price;
+
+      const container = document.createElement("div");
+      container.append(imagen, title, price);
+
+      todosLosItems.push(container);
     });
     document.body.append(...todosLosItems);
-})
-
+  });
