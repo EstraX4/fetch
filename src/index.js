f074c70419564d2acd6df7a985feb213a1f4daeb
Juan David Estrada Carvajal <Estraxclips@gmail.com>
2022-11-24 01:00:55 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const url = "https://platzi-avo.vercel.app/api/avo";
 
-console.log('Happy hacking :)')
+//web api
+//Conectamos al server
+window
+.fetch(url)
+//procesar la respuesta y convertirla en JSON
+.then((respuesta) => respuesta.json())
+// JSON -> Data -> Renderizar info browser
+.then((responseJson) => {
+    const todosLosItems = []
+    responseJson.data.forEach((item) => {
+        const imagen = document.createElement("img");
+        const title = document.createElement("h2");
+        const price = document.createElement("div");
+
+        const container = document.createElement("div");
+        container.append(imagen, title, price);
+
+        todosLosItems.push(container);
+    });
+    document.body.append(...todosLosItems);
+})
+
