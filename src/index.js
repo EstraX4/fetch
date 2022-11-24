88ea61beef24752ef1f3b3338cbe3544e1d06705
Juan David Estrada Carvajal <Estraxclips@gmail.com>
2022-11-24 17:50:24 -0500

    v

--- src/index.js
+++ src/index.js
@@ -1,30 +1,56 @@
-const baseUrl ="https://platzi-avo.vercel.app"
+const baseUrl = "https://platzi-avo.vercel.app";
+const mountNode = document.getElementById("js-mount");
 
-const appNode = document.querySelector("#app")
+const formatPrice = (price) =>
+  new Intl.NumberFormat("en-EN", {
+    style: "currency",
+    currency: "USD",
+  }).format(price);
 
-//web api
-//Conectamos al server
-window
-  .fetch(`${baseUrl}/api/avo`)
-  //procesar la respuesta y convertirla en JSON
-  .then((respuesta) => respuesta.json())
-  // JSON -> Data -> Renderizar info browser
-  .then((responseJson) => {
-    const todosLosItems = [];
-    responseJson.data.forEach((item) => {
-      const imagen = document.createElement("img");
-        imagen.src = `${baseUrl}${item.image}`;
+!(async function () {
+  const response = await fetch(`${baseUrl}/api/avo`);
+  // 💡 More about Spread: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+  const { data: allAvos } = await response.json();
 
-      const title = document.createElement("h2");
-      title.textContent = item.name;
+  // Create the HTML Nodes for each avocado we receive from the API
+  // 💡 More about Array.map: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+  const nodeArray = allAvos.map((avocado) => {
+    // Create image node
+    // <img class="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6" src="avatar.jpg">
+    const image = document.createElement("img");
+    image.className =
+      "h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6";
+    image.src = `${baseUrl}${avocado.image}`;
 
-      const price = document.createElement("div");
-      price.textContent = item.price;
+    // Create heading
+    // <h2 class="text-lg">Erin Lindford</h2>
+    const title = document.createElement("h2");
+    title.className = "text-lg";
+    title.textContent = avocado.name;
 
-      const container = document.createElement("div");
-      container.append(imagen, title, price);
+    // Create Price
+    // <div class="text-gray-600">(555) 765-4321</div>
+    const price = document.createElement("div");
+    price.className = "text-gray-600";
+    price.textContent = formatPrice(avocado.price);
 
-      todosLosItems.push(container);
-    });
-    appNode.append(...todosLosItems);
+    // Wrap price & title
+    // <div class="text-center md:text-left"><price ><title ></div>
+    const priceAndTitle = document.createElement("div");
+    priceAndTitle.className = "text-center md:text-left";
+    priceAndTitle.appendChild(title);
+    priceAndTitle.appendChild(price);
+
+    // Wrap Img and priceAndTitle
+    // <div class="md:flex bg-white rounded-lg p-6">
+    const card = document.createElement("div");
+    card.className = "md:flex bg-white rounded-lg p-6 hover:bg-gray-300";
+    card.appendChild(image);
+    card.appendChild(priceAndTitle);
+
+    return card;
   });
+
+  // Trick: Apply an array as a list of arguments
+  mountNode.append(...nodeArray);
+})();
